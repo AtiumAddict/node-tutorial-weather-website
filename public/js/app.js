23179b084652f1adc6509d3d05a034299ff4a502
Alexandros Lagios <alexandroslagios@gmail.com>
2022-03-05 16:52:11 +0200

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -3,22 +3,21 @@ const search = document.querySelector('input');
 const messageOne = document.querySelector('#message-1');
 const messageTwo = document.querySelector('#message-2');
 
-messageOne.textContent = 'Loading...';
-messageTwo.textContent = '';
-
 weatherForm.addEventListener('submit', (e) => {
   e.preventDefault();
   const locationParam = search.value;
-  fetch('http://localhost:3000/weather?address=' + locationParam).then(
-    (res) => {
-      res.json().then((data) => {
-        if (data.error) {
-          console.log(data.error);
-        } else {
-          messageOne.textContent = data.location;
-          messageTwo.textContent = data.forecast;
-        }
-      });
-    }
-  );
+
+  messageOne.textContent = 'Loading...';
+  messageTwo.textContent = '';
+
+  fetch('/weather?address=' + locationParam).then((res) => {
+    res.json().then((data) => {
+      if (data.error) {
+        messageOne.textContent = data.error;
+      } else {
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forecast;
+      }
+    });
+  });
 });
